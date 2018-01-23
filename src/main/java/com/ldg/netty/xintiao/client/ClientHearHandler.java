23c9000bee274cf03825d9d67589c4506635ff46
liudongguang <liudongguang@qq.com>
2018-01-23 20:17:35 +0800

    2018-01-23

--- src/main/java/com/ldg/netty/xintiao/client/ClientHearHandler.java
+++ src/main/java/com/ldg/netty/xintiao/client/ClientHearHandler.java
@@ -1,14 +1,23 @@
 package com.ldg.netty.xintiao.client;
 
+import com.ldg.netty.xintiao.Constant;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 
-public class ClientHearHandler  extends ChannelInboundHandlerAdapter {
+public class ClientHearHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        ByteBuf byteBuf=(ByteBuf)msg;
-        System.out.println("type:"+byteBuf.getByte(4)+msg);
-        ctx.fireChannelRead(msg);
+        ByteBuf byteBuf = (ByteBuf) msg;
+        //System.out.println("type:" + byteBuf.getByte(4) + msg);
+        switch (byteBuf.getByte(4)) {
+            case 1:
+                ctx.writeAndFlush(Constant.cbeat);
+                System.out.println("收到服务端心跳！");
+                break;
+            default:
+                ctx.fireChannelRead(msg);
+        }
+
     }
 }
--- src/main/java/com/ldg/netty/xintiao/server/ServerHeartHandler.java
+++ src/main/java/com/ldg/netty/xintiao/server/ServerHeartHandler.java
@@ -2,6 +2,7 @@ package com.ldg.netty.xintiao.server;
 
 import com.ldg.netty.xintiao.Constant;
 import com.ldg.netty.xintiao.MessageProtocol;
+import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.handler.timeout.IdleState;
@@ -10,6 +11,18 @@ import io.netty.handler.timeout.IdleStateEvent;
 import java.util.UUID;
 
 public class ServerHeartHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf byteBuf = (ByteBuf) msg;
+        //System.out.println("type:" + byteBuf.getByte(4) + msg);
+        switch (byteBuf.getByte(4)) {
+            case 2:
+                System.out.println("收到客户端心跳！");
+                break;
+            default:
+                ctx.fireChannelRead(msg);
+        }
+    }
 
     @Override
     public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
